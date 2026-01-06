bf18d464297358f7bad88dd5fac8ef654a6b5103
amitkumar223 <amit.ittechxpert1234@gmail.com>
2026-01-06 11:05:06 +0530

    new

---
+++ 01-folder/src/components/Card.jsx
@@ -0,0 +1,12 @@
+function card () {
+    return (
+    <div>
+        <div className="card">
+            <h2>This is a card component</h2>
+            <p>Card content goes here.</p>
+        </div>; 
+    </div>
+    );
+    }
+
+    export default card;
---
+++ 01-folder/src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+      <div className="navbar">
+        <h1> this is navbar</h1>
+      </div>
+    </div>
+  )
+}
+
+export default navbar
